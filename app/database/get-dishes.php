033a2886365a353777bd53a261959f677451e4cf
sp808 <sp808@disroot.org>
2026-07-24 01:04:49 +0300

    header fix

--- app/database/get-dishes.php
+++ app/database/get-dishes.php
@@ -1,5 +1,8 @@
 ﻿<?php
 // Set headers for JSON response
+header('Access-Control-Allow-Origin: *'); 
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Content-Type: application/json');
 
 try {
