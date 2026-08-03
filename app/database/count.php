db8bd5d39aa1c87c256e2d4fb6a3f6c2c7f39663
sp808 <sp808@disroot.org>
2026-08-03 21:22:04 +0200

    getdbconnection func

--- app/database/count.php
+++ app/database/count.php
@@ -4,6 +4,7 @@ header('Content-Type: application/json; charset=utf-8');
 try {
     require_once __DIR__ . '/../connection.php';
     
+	$pdo = getDbConnection();
     $sql = "SELECT COUNT(*) FROM mainbase";
     $stmt = $pdo->prepare($sql);
     $stmt->execute();
@@ -16,7 +17,7 @@ try {
     http_response_code(500);
     echo json_encode([
         'error' => true,
-        'message' => 'Ошибка при выборке данных из БД'
+        'message' => 'Error when selecting from DB'
     ], JSON_UNESCAPED_UNICODE);
 }
 ?>
