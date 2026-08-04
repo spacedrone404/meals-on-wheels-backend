202b972e1a98f2d519fb51c474fcbac5af961c3d
sp808 <sp808@disroot.org>
2026-08-04 20:32:56 +0200

    get-dishes-test

--- app/connection.php
+++ app/connection.php
@@ -2,13 +2,12 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json; charset=utf-8');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
     exit;
 }
-
-header('Content-Type: application/json');
 
 function getDbConnection(): PDO {
     static $pdo = null;
--- app/database/get-dishes.php
+++ app/database/get-dishes.php
@@ -1,40 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *'); 
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(204);
-    exit;
-}
-
-header('Content-Type: application/json');
+require_once __DIR__ . '/../connection.php';
 
 try {
-    $db_env = getenv('DATABASE_URL');
-    if (!$db_env) {
-        throw new Exception("Error: DATABASE_URL environment variable is not set.");
-    }
-
-    $db_parsed = parse_url($db_env);
-
-    $host     = $db_parsed["host"] ?? 'localhost';
-    $port     = $db_parsed["port"] ?? 5432;
-    $user     = $db_parsed["user"] ?? '';
-    $password = $db_parsed["pass"] ?? ''; 
-    $dbname   = ltrim($db_parsed["path"] ?? '', "/"); 
-
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;client_encoding=UTF8";
-
-    $pdo = new PDO($dsn, $user, $password, [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false
-    ]);
-
-    $pdo->exec("SET client_encoding TO 'UTF8'");
-
+    $pdo = getDbConnection();
     $sql = 'SELECT *
             FROM mainbase
             ORDER BY CASE 
